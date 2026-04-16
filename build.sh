e8f539ee6a4d877b298e21a8505b1ebbff771fa2
ravindu644 <droidcasts@protonmail.com>
2026-04-16 13:26:51 +0530

    introduced a simple build script
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

---
+++ build.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+echo -e "\n[INFO]: BUILD STARTED..!\n"
+
+#init submodules
+git submodule update --init --recursive || true
+
+export KERNEL_ROOT="$(pwd)"
+export ARCH=arm64
+export KBUILD_BUILD_USER="@ravindu644"
+
+mkdir -p "${KERNEL_ROOT}/out" "${KERNEL_ROOT}/build" "${HOME}/toolchains"
+
+# init clang-r383902b
+if [ ! -d "${HOME}/toolchains/clang-r383902b" ]; then
+    echo -e "\n[INFO] Cloning clang-r383902b...\n"
+    mkdir -p "${HOME}/toolchains/clang-r383902b" && cd "${HOME}/toolchains/clang-r383902b"
+    curl -LO "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/0e9e7035bf8ad42437c6156e5950eab13655b26c/clang-r383902b.tar.gz"
+    tar -xf clang-r383902b.tar.gz && rm clang-r383902b.tar.gz
+    cd "${KERNEL_ROOT}"
+fi
+
+# init aarch64-linux-android-4.9-Linux-5.4/
+if [ ! -d "${HOME}/toolchains/aarch64-linux-android-4.9-Linux-5.4" ]; then
+    echo -e "\n[INFO] Cloning aarch64-linux-android-4.9-Linux-5.4\n"
+    mkdir -p "${HOME}/toolchains/aarch64-linux-android-4.9-Linux-5.4" && cd "${HOME}/toolchains/aarch64-linux-android-4.9-Linux-5.4"
+    curl -LO "https://github.com/ravindu644/Android-Kernel-Tutorials/releases/download/toolchains/aarch64-linux-android-4.9-Linux-5.4.tar.gz"
+    tar -xf aarch64-linux-android-4.9-Linux-5.4.tar.gz && \
+        rm aarch64-linux-android-4.9-Linux-5.4.tar.gz
+    cd "${KERNEL_ROOT}"
+fi
+
+# Export toolchain paths
+export PATH="${HOME}/toolchains/clang-r383902b/bin:${PATH}"
+export LD_LIBRARY_PATH="${HOME}/clang-r383902b/lib:${HOME}/clang-r383902b/lib64:${LD_LIBRARY_PATH}"
+
+# Set cross-compile environment variables
+export BUILD_CROSS_COMPILE="${HOME}/toolchains/aarch64-linux-android-4.9-Linux-5.4/bin/aarch64-linux-android-"
+export BUILD_CC="${HOME}/toolchains/clang-r383902b/bin/clang"
+
+# Build options for the kernel
+export BUILD_OPTIONS=(
+    HOSTLDLIBS="-lyaml"
+    -C "${KERNEL_ROOT}"
+    O="${KERNEL_ROOT}/out"
+    -j"$(nproc)"
+    ARCH=arm64
+    CROSS_COMPILE="${BUILD_CROSS_COMPILE}"
+    CC="${BUILD_CC}"
+    CLANG_TRIPLE=aarch64-linux-gnu-
+)
+
+build_kernel(){
+    # Cleanup
+    # make "${BUILD_OPTIONS[@]}" clean && make "${BUILD_OPTIONS[@]}" mrproper
+    
+    # Make default configuration.
+    make "${BUILD_OPTIONS[@]}" beyond1qlte_jpn_kdi_defconfig
+
+    # Configure the kernel (GUI)
+    make "${BUILD_OPTIONS[@]}" menuconfig
+
+    # Build the kernel
+    make "${BUILD_OPTIONS[@]}" Image || exit 1
+
+    # Copy the built kernel to the build directory
+    cp "${KERNEL_ROOT}/out/arch/arm64/boot/Image" "${KERNEL_ROOT}/build"
+
+    echo -e "\n[INFO]: BUILD FINISHED..!"
+}
+build_kernel
