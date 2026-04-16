15f3e4fe8403fb8d81768f0a604a5dc6e3689bf9
ravindu644 <droidcasts@protonmail.com>
2026-04-16 15:55:40 +0530

    defconfig: enabled Droidspaces support
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- build.sh
+++ build.sh
@@ -56,7 +56,7 @@ build_kernel(){
     # make "${BUILD_OPTIONS[@]}" clean && make "${BUILD_OPTIONS[@]}" mrproper
     
     # Make default configuration.
-    make "${BUILD_OPTIONS[@]}" beyond1qlte_jpn_kdi_defconfig custom.config
+    make "${BUILD_OPTIONS[@]}" beyond1qlte_jpn_kdi_defconfig custom.config droidspaces.config
 
     # Configure the kernel (GUI)
     make "${BUILD_OPTIONS[@]}" menuconfig
